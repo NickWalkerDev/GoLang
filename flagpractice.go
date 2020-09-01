c73f3ed2baf689d0d3e9fbf9aa1ae15bcda1d6b7
Nick Walker <nickwalkersoftwareengineer@gmail.com>
2020-08-31 22:02:31 -0700

    Add files via upload

--- flagpractice.go
+++ flagpractice.go
@@ -3,10 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var limitFlag = flag.Int("limit", 1000, "This is the limit of lines per file")
+	addcmd := flag.NewFlagSet("add", flag.ExitOnError)
+	a_add := addcmd.Int("a", 0, "The value of a")
+	b_add := addcmd.Int("b", 5, "The value of b")
+
+	mulcmd := flag.NewFlagSet("mul", flag.ExitOnError)
+	a_mul := mulcmd.Int("a", 147, "The value of a")
+	b_mul := mulcmd.Int("b", 234, "The value of b")
 	flag.Parse()
-	fmt.Println(*limitFlag)
+
+	switch os.Args[1] {
+	case "add":
+		addcmd.Parse(os.Args[2:])
+		fmt.Println(*a_add + *b_add)
+	case "mul":
+		mulcmd.Parse(os.Args[2:])
+		fmt.Println(*(a_mul) * (*b_mul))
+	default:
+		fmt.Println("expected 'add' or 'mul' command")
+		os.Exit(1)
+	}
+
 }
